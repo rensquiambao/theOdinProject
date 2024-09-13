6f513ecca3727f1784803222ed8b98bb14ddaa27
Rens Q <anoyorenelyn@gmail.com>
2024-09-13 19:13:07 +0800

    added start button

--- script.js
+++ script.js
@@ -3,38 +3,69 @@ let computerScore = 0;
 
 const humanScoreElem = document.querySelector("#humanScoreElem");
 const computerScoreElem = document.querySelector("#computerScoreElem");
-const winnerAnnouncer = document.querySelector("#winnerAnnouncer");
+const messageLog = document.querySelector("#message-log");
+const humanChoiceimg = document.querySelector("#img-humanChoice");
+const computerChoiceImg = document.querySelector("#img-computerChoice");
 const buttons = document.querySelectorAll(".img-btn");
+const startBtn = document.querySelector("#start-btn");
 
-winnerAnnouncer.textContent = "PRESS CHOICE TO START";
+messageLog.textContent = "ROCK, PAPER, SCISSORS";
+
+buttons.forEach((button) => {
+    button.classList.add("disabled");
+});
+
+function resetGame() {
+    buttons.forEach((button) => {
+        button.classList.remove("disabled");
+    });
+
+    humanScore = 0;
+    computerScore = 0;
+    humanScoreElem.textContent = humanScore;
+    computerScoreElem.textContent = computerScore;
+    messageLog.textContent = "GAME START";
+    messageLog.textContent = "Press your choice to start";
+
+    startBtn.disabled = true;
+}
+startBtn.addEventListener("click", resetGame);
 
 const btnRock = document.querySelector("#btnRock");
 btnRock.addEventListener("click", () => {
     const humanChoice = "rock";
+    const humanChoiceimg = document.querySelector("#img-humanChoice");
+    humanChoiceimg.src = "./images/hand rock.png";
     playRound(humanChoice, getComputerChoice());
 });
 
 const btnPaper = document.querySelector("#btnPaper");
 btnPaper.addEventListener("click", () => {
     const humanChoice = "paper";
+    const humanChoiceimg = document.querySelector("#img-humanChoice");
+    humanChoiceimg.src = "./images/hand-paper.png";
     playRound(humanChoice, getComputerChoice());
 });
 
 const btnScsrs = document.querySelector("#btnScsrs");
 btnScsrs.addEventListener("click", () => {
     const humanChoice = "scissors";
+    const humanChoiceimg = document.querySelector("#img-humanChoice");
+    humanChoiceimg.src = "./images/hand scissors.png";
     playRound(humanChoice, getComputerChoice());
 });
 
 function getComputerChoice() {
-    computerSelection = Math.floor(Math.random() * 3);
-
+    let computerSelection = Math.floor(Math.random() * 3);
     if (computerSelection === 0) {
         computerSelection = "rock";
+        computerChoiceImg.src = "./images/hand rock.png";
     } else if (computerSelection === 1) {
         computerSelection = "paper";
+        computerChoiceImg.src = "./images/hand-paper.png";
     } else if (computerSelection === 2) {
         computerSelection = "scissors";
+        computerChoiceImg.src = "./images/hand scissors.png";
     }
     return computerSelection;
 }
@@ -44,13 +75,13 @@ function playRound(humanChoice, computerChoice) {
     console.log(computerChoice);
 
     if (humanChoice === computerChoice) {
-        winnerAnnouncer.textContent = "It's a tie. Try Again.";
+        messageLog.textContent = "It's a tie. Try Again.";
     } else if (
         (humanChoice === "paper" && computerChoice === "rock") ||
         (humanChoice === "rock" && computerChoice === "scissors") ||
         (humanChoice === "scissors" && computerChoice === "paper")
     ) {
-        winnerAnnouncer.textContent = "You win";
+        messageLog.textContent = "You win";
         humanScore++;
         humanScoreElem.textContent = humanScore;
     } else if (
@@ -58,24 +89,25 @@ function playRound(humanChoice, computerChoice) {
         (computerChoice === "rock" && humanChoice === "scissors") ||
         (computerChoice === "scissors" && humanChoice === "paper")
     ) {
-        winnerAnnouncer.textContent = "You lose";
+        messageLog.textContent = "You lose";
         computerScore++;
         computerScoreElem.textContent = computerScore;
     } else {
-        winnerAnnouncer.textContent = "ERROR";
+        messageLog.textContent = "ERROR";
     }
     console.log("Human", humanScore);
     console.log("Computer", computerScore);
 
     function endGame(winnerMessage) {
         buttons.forEach((button) => {
-            button.disabled = true;
+            button.classList.add("disabled");
         });
-        winnerAnnouncer.textContent = winnerMessage;
+        messageLog.textContent = winnerMessage;
+        startBtn.disabled = false;
     }
 
     if (humanScore === 5) {
-        endGame("Congratulations! You win the game!");
+        endGame("Congratulations, You win!");
     } else if (computerScore === 5) {
         endGame("You lost. Play Again");
     }
